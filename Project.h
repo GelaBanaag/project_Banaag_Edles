d41a88a54bf0357f6670a928ad64bb46ad8356df
Lara Mae Edles <laramae_edles@yahoo.com>
2013-02-15 08:50:03 +0800

    Project

---
+++ Project.h
@@ -0,0 +1,25 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+
+class Project
+{
+    public:
+        Project();
+        virtual ~Project();
+        bool isPrime(int num);
+        void multiples_of_3_and_5(); //problem 1
+        void even_fibonacce(); //problem 2
+        void largest_prime_factor(); //problem 3
+        void largest_palindrome(); //problem 4
+        void smallest_multiple(); //problem 5
+        void sum_square_diff(); //problem 6
+        void _10001st_prime(); //problem 7
+        void largest_product_in_series(); //problem 8
+        void special_pytho_triplet(); //problem 9
+        void summation_of_primes(); //problem 10
+    protected:
+    private:
+};
+
+#endif // PROJECT_H
